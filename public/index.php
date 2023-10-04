8332ce2d2c16b5118ad93fbc0ee78fb33059d077
nadirspam <69924442+nadirspam@users.noreply.github.com>
2023-10-04 12:20:26 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+$app = include __DIR__. "/../src/App/bootstrap.php";
+
+$app->run();
---
+++ src/Framework/App.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Framework;
+
+class App{
+    public function run(){
+        echo "<h1>Application is running 2</h1>";
+    }
+}
